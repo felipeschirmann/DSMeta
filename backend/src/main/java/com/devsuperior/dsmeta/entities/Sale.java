0562aa985ad005499028d9c0f7f1848c0c5409c7
Felipe Schirmann <contato@felipeschirmann.dev.br>
2023-05-29 18:49:43 -0300

    fix org.hibernate.LazyInitializationException: could not initialize proxy in dev profile

--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -23,6 +23,15 @@ public class Sale {
 	
 	public Sale() {
 	}
+	
+	public Sale(Long id, String sellerName, Integer visited, Integer deals, Double amount, LocalDate date) {
+		this.id = id;
+		this.sellerName = sellerName;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+	}
 
 	public Long getId() {
 		return id;
