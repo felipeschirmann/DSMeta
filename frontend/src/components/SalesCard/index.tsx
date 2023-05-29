966e75081bf27005f892e5826f19a48ec5cbd606
Felipe Schirmann <contato@felipeschirmann.dev.br>
2023-05-29 16:07:44 -0300

    Axios, useEffect first request

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -3,13 +3,21 @@ import "react-datepicker/dist/react-datepicker.css";
 import NotificationButton from "../NotificationButton";
 
 import "./styles.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function SalesCard() {
   const min = new Date(new Date().setDate(new Date().getDate() - 365));
   const max = new Date();
   const [minDate, setMinDate] = useState(min);
   const [maxDate, setMaxDate] = useState(max);
+
+  useEffect(() => {
+    axios.get("http://localhost:8080/sales")
+    .then( response => {
+      console.log(response.data);
+    })
+  }, []);
 
   return (
     <>
