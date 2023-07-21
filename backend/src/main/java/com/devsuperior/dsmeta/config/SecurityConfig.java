a55094f0fafc726056b0066774717a0043d9c128
felipeschirmann <contato@felipeschirmann.dev.br>
2023-07-21 17:43:52 -0300

    Ajusts backend security

--- backend/src/main/java/com/devsuperior/dsmeta/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsmeta/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package com.devsuperior.dsmeta.config;
 
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,11 +17,16 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+	private Environment env;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.headers().frameOptions().disable();
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			http.headers().frameOptions().disable();
+		}
+
         http.cors().and().csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.authorizeHttpRequests((auth) -> auth.anyRequest().permitAll());
